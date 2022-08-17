30e67ad0d7877037b4b4b7dff6ea970eeba0cfbe
Dantas <TecnicoSuporte02@tupperware.com>
2022-08-17 17:25:39 -0300

    170820227

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Header } from './Header';
 export { default as ArrowBack } from './ArrowBack';
 export { default as Donutbox } from './Donutbox';
 export { default as Contact } from './Contact';
-export { default as ApiMap } from './ApiMap';
 export { default as Footer } from './Footer';
